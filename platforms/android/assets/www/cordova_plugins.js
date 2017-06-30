ad0535a50c5bf4f82edfed0993135fbd3dcc9e15
armstrob <bruce.armstrong88@gmail.com>
2017-06-30 08:22:38 -0400

    Android Scandit
    Blank PG project, added scandit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,122 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "com.mirasense.scanditsdk.plugin.Barcode",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/barcode.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.Barcode"
+        ]
+    },
+    {
+        "id": "com.mirasense.scanditsdk.plugin.BarcodePicker",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/barcodepicker.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.BarcodePicker"
+        ]
+    },
+    {
+        "id": "com.mirasense.scanditsdk.plugin.License",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/license.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.License"
+        ]
+    },
+    {
+        "id": "com.mirasense.scanditsdk.plugin.Margins",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/margins.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.Margins"
+        ]
+    },
+    {
+        "id": "com.mirasense.scanditsdk.plugin.Point",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/point.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.Point"
+        ]
+    },
+    {
+        "id": "com.mirasense.scanditsdk.plugin.Quadrilateral",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/quadrilateral.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.Quadrilateral"
+        ]
+    },
+    {
+        "id": "com.mirasense.scanditsdk.plugin.RecognizedText",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/recognizedtext.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.RecognizedText"
+        ]
+    },
+    {
+        "id": "com.mirasense.scanditsdk.plugin.Rect",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/rect.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.Rect"
+        ]
+    },
+    {
+        "id": "com.mirasense.scanditsdk.plugin.ScanOverlay",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/overlay.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.ScanOverlay"
+        ]
+    },
+    {
+        "id": "com.mirasense.scanditsdk.plugin.ScanSession",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/session.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.ScanSession"
+        ]
+    },
+    {
+        "id": "com.mirasense.scanditsdk.plugin.ScanSettings",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/settings.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.ScanSettings"
+        ]
+    },
+    {
+        "id": "com.mirasense.scanditsdk.plugin.SymbologySettings",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/symbologysettings.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.SymbologySettings"
+        ]
+    },
+    {
+        "id": "com.mirasense.scanditsdk.plugin.ScanCase",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/scancase.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.ScanCase"
+        ]
+    },
+    {
+        "id": "com.mirasense.scanditsdk.plugin.Constraints",
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/constraints.js",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.Constraints"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.mirasense.scanditsdk.plugin": "5.3.3"
+};
+// BOTTOM OF METADATA
+});
---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,122 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/barcode.js",
+        "id": "com.mirasense.scanditsdk.plugin.Barcode",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.Barcode"
+        ]
+    },
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/barcodepicker.js",
+        "id": "com.mirasense.scanditsdk.plugin.BarcodePicker",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.BarcodePicker"
+        ]
+    },
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/license.js",
+        "id": "com.mirasense.scanditsdk.plugin.License",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.License"
+        ]
+    },
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/margins.js",
+        "id": "com.mirasense.scanditsdk.plugin.Margins",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.Margins"
+        ]
+    },
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/point.js",
+        "id": "com.mirasense.scanditsdk.plugin.Point",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.Point"
+        ]
+    },
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/quadrilateral.js",
+        "id": "com.mirasense.scanditsdk.plugin.Quadrilateral",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.Quadrilateral"
+        ]
+    },
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/recognizedtext.js",
+        "id": "com.mirasense.scanditsdk.plugin.RecognizedText",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.RecognizedText"
+        ]
+    },
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/rect.js",
+        "id": "com.mirasense.scanditsdk.plugin.Rect",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.Rect"
+        ]
+    },
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/overlay.js",
+        "id": "com.mirasense.scanditsdk.plugin.ScanOverlay",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.ScanOverlay"
+        ]
+    },
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/session.js",
+        "id": "com.mirasense.scanditsdk.plugin.ScanSession",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.ScanSession"
+        ]
+    },
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/settings.js",
+        "id": "com.mirasense.scanditsdk.plugin.ScanSettings",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.ScanSettings"
+        ]
+    },
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/symbologysettings.js",
+        "id": "com.mirasense.scanditsdk.plugin.SymbologySettings",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.SymbologySettings"
+        ]
+    },
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/scancase.js",
+        "id": "com.mirasense.scanditsdk.plugin.ScanCase",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.ScanCase"
+        ]
+    },
+    {
+        "file": "plugins/com.mirasense.scanditsdk.plugin/src/constraints.js",
+        "id": "com.mirasense.scanditsdk.plugin.Constraints",
+        "pluginId": "com.mirasense.scanditsdk.plugin",
+        "clobbers": [
+            "Scandit.Constraints"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.mirasense.scanditsdk.plugin": "5.3.3"
+}
+// BOTTOM OF METADATA
+});
